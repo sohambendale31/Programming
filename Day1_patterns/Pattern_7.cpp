a218231463ef1903966e2491e3f402689c2a1df2
Soham Bendale <sohambendale31@gmail.com>
2022-06-11 18:00:06 +0530

    Update Pattern_7.cpp

--- Day1_patterns/Pattern_7.cpp
+++ Day1_patterns/Pattern_7.cpp
@@ -18,13 +18,11 @@ int main(){
     int row, counter=0;
 
     cin>>row;
-    int number=row*row;
     while(counter<row){
         int column=0;
         while(column<=counter){
             cout<<"* ";
             column++;
-            number--;
         }
         counter++;
         cout<<endl;
